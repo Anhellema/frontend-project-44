62c057c00ee3dddc10f98c126b7fd60611867200
Anhel <anhellema@gmail.com>
2025-10-28 22:52:48 +0300

    a little fix to gameType object, brainPrime rules

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ const gameType = {
     getCorrectAnswerValue: () => correctAnswerVariable, // получаем измененную переменную из глобального окружения
   },
   brainPrime: {
-    gameRules: 'Answer "yes" if the number is even, otherwise answer "no".',
+    gameRules: 'Answer "yes" if given number is prime. Otherwise answer "no".',
     getGameQuestion: () => [getRandomNumber()],
     getCorrectAnswerValue: (q) => isPrime(q),
   },
